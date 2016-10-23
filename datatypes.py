18e1a895b64261570e04082cb272a27472a8db10
Derek Jobst <derekjobst@gmail.com>
2016-10-23 17:48:35 -0400

    __init_ on Nat

--- datatypes.py
+++ datatypes.py
@@ -4,23 +4,44 @@ _B = 4
 def size(num):
     return num.bit_length()
 
+def binary(i):
+    return format(i, '04b')
+
+def breakup(i, s):
+    for j in xrange(len(i), 0, -s):
+        if j < s:
+            yield i[:j]
+        else:
+            yield i[j-s:j]
+
+
 class Word(object):
 
     word = None
 
-    def __init__(self, w):
+    def __init__(self, w, base=10):
+        w = int(str(w), base) # Make sure w is an int
         if w < 0:
             raise Exception("Error: Negative input")
 
         if size(w) > _B:
-            raise Exception("Error: Overflow")
+            raise Exception("Error: Overflow with w: {}".format(w))
 
         self.word = w
 
     def __str__(self):
-        return str(self.word)
+        return binary(self.word)
 
     def __int__(self):
         return self.word
 
 
+class Nat(object):
+
+    nat = []
+
+    def __init__(self, n):
+        n = int(n)
+        num = binary(n)
+        print num
+        self.nat = [Word(x, 2) for x in breakup(num, _B)]
--- datatypes_test.py
+++ datatypes_test.py
@@ -1,4 +1,4 @@
-from datatypes import _B, Word
+from datatypes import _B, Word, breakup, Nat
 
 print "_B size is: {}".format(_B)
 
@@ -6,8 +6,16 @@ print "_B size is: {}".format(_B)
 
 w1 = Word(0)
 assert w1.word == 0
-assert str(w1) == '0'
+assert str(w1) == '0000'
 assert int(w1) == 0
 assert type(w1) == Word
 
-w2 = Word(12345)
+# print list(breakup('1234567890', 4))
+
+# Test Nat
+
+n1 = Nat(0)
+print [str(n) for n in n1.nat]
+
+n1 = Nat(894)
+print [str(n) for n in n1.nat]
